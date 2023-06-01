973fe335b07d017f4fb018d420c53bcc6f3ab441
sulaimondawood <sulaimondawod@gmail.com>
2023-06-01 03:14:18 +0100

    last commit befre going to bed

--- sanity/details2.ts
+++ sanity/details2.ts
@@ -73,5 +73,101 @@ export default {
       title: "Sec Text",
       type: "string",
     },
+    {
+      name: "div1Bg",
+      title: "Div1 Bg",
+      type: "string",
+    },
+    {
+      name: "div1Text",
+      title: "Div1 Text",
+      type: "string",
+    },
+    {
+      name: "div1Image",
+      title: "Div1 Image",
+      type: "image",
+    },
+    {
+      name: "div2ItemBgColor",
+      title: "Div2 Item BgColor",
+      type: "string",
+    },
+    {
+      name: "div2ItemText",
+      title: "Div2 Item Text",
+      type: "string",
+    },
+    {
+      name: "div2ItemTextColor",
+      title: "Div2 Item Text Color",
+      type: "string",
+    },
+    {
+      name: "div2Item2Image",
+      title: "Div2 Item2 Image",
+      type: "image",
+    },
+    {
+      name: "div2Item2Text",
+      title: "Div2 Item2 Text",
+      type: "string",
+    },
+    {
+      name: "div2Item2TextColor",
+      title: "Div2 Item2 Text Color",
+      type: "string",
+    },
+    {
+      name: "div3BgColor",
+      title: "Div3 BgColor",
+      type: "string",
+    },
+    {
+      name: "div3Title",
+      title: "Div3 Title",
+      type: "string",
+    },
+    {
+      name: "div3TitleColor",
+      title: "Div3 Title Color",
+      type: "string",
+    },
+    {
+      name: "div3Image",
+      title: "Div3 Image",
+      type: "image",
+    },
+    {
+      name: "div4Item1Text",
+      title: "Div4 Item1 Text",
+      type: "string",
+    },
+    {
+      name: "div4Item1TextColor",
+      title: "Div4 Item1 Text Color",
+      type: "string",
+    },
+    {
+      name: "div4Item2BgColor",
+      title: "Div4 Item2 BgColor ",
+      type: "string",
+    },
+    {
+      name: "div4Item2Text",
+      title: "Div4 Item2 Text ",
+      type: "string",
+    },
+    {
+      name: "div4Item2TextColor",
+      title: "Div4 Item2 Text Color",
+      type: "string",
+    },
+    {
+      name: "div4Item2Image",
+      title: "Div4 Item2 Image",
+      type: "array",
+      of: [{ type: "image" }],
+    },
   ],
 };
--- src/app/card/[card]/page.tsx
+++ src/app/card/[card]/page.tsx
@@ -43,6 +43,55 @@ const page = async ({ params }: { params: { card: string } }) => {
         </p>
         <p>{data.secText}</p>
       </article>
+      <article
+        style={{ backgroundColor: data.div1Bg }}
+        className={styles.grid1}
+      >
+        <h2>{data.div1Text}</h2>
+        <img src={urlForImage(data.div1Image).url()} alt="" />
+      </article>
+      <article className={styles.grid_2}>
+        <div
+          style={{ backgroundImage: data.div2ItemBgColor }}
+          className={styles.left}
+        >
+          <h1 style={{ color: data.div2ItemTextColor }}>{data.div2ItemText}</h1>
+        </div>
+        <div className={styles.right}>
+          <img src={urlForImage(data.div2Item2Image).url()} alt="" />
+          <h1 style={{ color: data.div2Item2TextColor }}>
+            {data.div2Item2Text}
+          </h1>
+        </div>
+      </article>
+      <article
+        style={{ backgroundColor: data.div3BgColor }}
+        className={styles.grid_3}
+      >
+        <img src={urlForImage(data.div3Image).url()} alt="" />
+        <h1 style={{ color: data.div3TitleColor }}>{data.div3Title}</h1>
+      </article>
+      <article className={styles.grid_4}>
+        <div className={styles.left}>
+          <h1 style={{ backgroundImage: data.div4Item1TextColor }}>
+            {data.div4Item1Text}
+          </h1>
+        </div>
+        <div
+          style={{ backgroundImage: data.div4Item2BgColor }}
+          className={styles.right}
+        >
+          <h1 style={{ color: data.div4Item2TextColor }}>
+            {data.div4Item2Text}
+          </h1>
+          <img
+            className={styles.img}
+            src={urlForImage(data.div4Item2Image[0]).url()}
+            alt=""
+          />
+          <img src={urlForImage(data.div4Item2Image[1]).url()} alt="" />
+        </div>
+      </article>
     </section>
   );
 };
