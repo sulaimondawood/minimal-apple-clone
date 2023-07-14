fd083b84e54dfb54646c9fc86564180cc159efdc
sulaimondawood <sulaimondawod@gmail.com>
2023-07-14 16:37:58 +0100

    modified nav

--- src/app/components/nav/Nav.tsx
+++ src/app/components/nav/Nav.tsx
@@ -13,6 +13,7 @@ import { gsap } from "gsap";
 import { useAppSelector } from "@/redux/hooks/hook";
 
 import { AiOutlineClose } from "react-icons/ai";
+import { useRouter } from "next/navigation";
 
 const Nav = ({
   state,
@@ -23,44 +24,22 @@ const Nav = ({
   bgState: string;
   colorState: string;
 }) => {
+  const router = useRouter();
   const { products } = useAppSelector((state) => state.basketSlice);
 
   const [isOpen, setIsOpen] = useState(false);
   const btnRef = useRef(null);
   const mobileRef = useRef(null);
 
-  let tl: any;
-  // const handleNav = useCallback((): void => {
-  //   const doc: any = document.querySelector("body");
-  //   if (isOpen) {
-  //     setIsOpen(!isOpen);
-  //     tl.play();
-  //     doc.style.overflow = "hidden";
-  //   } else {
-  //     setIsOpen(!isOpen);
-  //     tl.reverse();
-  //     doc.style.overflow = "auto";
-  //   }
-  // }, [isOpen]);
-  // useLayoutEffect(() => {
-  //   tl = gsap.timeline({ paused: true });
-  //   tl.from(mobileRef.current, {
-  //     yPercent: -100,
-  //     opacity: 0,
-  //     ease: "Power2.out",
-  //   });
-  //   tl.to(mobileRef.current, {
-  //     yPercent: 0,
-  //     opacity: 1,
-  //     duration: 1,
-  //     ease: "Power2.out",
-  //   });
-  // }, [isOpen]);
   const onClose = () => {
     setIsOpen(false);
   };
   const onOpen = () => {
     setIsOpen(true);
+  };
+
+  const handleRoute = () => {
+    router.push("/shop/bag");
   };
 
   useEffect(() => {
@@ -136,7 +115,7 @@ const Nav = ({
             ></path>
           </svg>
         </button>
-        <Link href="/shop/bag" className={styles.svg_grd}>
+        <button className={styles.svg_grd} onClick={handleRoute}>
           {products.length > 0 && (
             <span style={{ color: bgState, backgroundColor: colorState }}>
               {products.length}
@@ -148,7 +127,20 @@ const Nav = ({
               d="m11.3535 16.0283h-1.0205a3.4229 3.4229 0 0 0 -3.333-2.9648 3.4229 3.4229 0 0 0 -3.333 2.9648h-1.02a2.1184 2.1184 0 0 0 -2.117 2.1162v7.7155a2.1186 2.1186 0 0 0 2.1162 2.1167h8.707a2.1186 2.1186 0 0 0 2.1168-2.1167v-7.7155a2.1184 2.1184 0 0 0 -2.1165-2.1162zm-4.3535-1.8652a2.3169 2.3169 0 0 1 2.2222 1.8652h-4.4444a2.3169 2.3169 0 0 1 2.2222-1.8652zm5.37 11.6969a1.0182 1.0182 0 0 1 -1.0166 1.0171h-8.7069a1.0182 1.0182 0 0 1 -1.0165-1.0171v-7.7155a1.0178 1.0178 0 0 1 1.0166-1.0166h8.707a1.0178 1.0178 0 0 1 1.0164 1.0166z"
             ></path>
           </svg>
-        </Link>
+        </button>
+        {/* <Link href="/shop/bag" className={styles.svg_grd}>
+          {products.length > 0 && (
+            <span style={{ color: bgState, backgroundColor: colorState }}>
+              {products.length}
+            </span>
+          )}
+          <svg viewBox="0 0 14 44" xmlns="http://www.w3.org/2000/svg">
+            <path
+              style={{ fill: colorState }}
+              d="m11.3535 16.0283h-1.0205a3.4229 3.4229 0 0 0 -3.333-2.9648 3.4229 3.4229 0 0 0 -3.333 2.9648h-1.02a2.1184 2.1184 0 0 0 -2.117 2.1162v7.7155a2.1186 2.1186 0 0 0 2.1162 2.1167h8.707a2.1186 2.1186 0 0 0 2.1168-2.1167v-7.7155a2.1184 2.1184 0 0 0 -2.1165-2.1162zm-4.3535-1.8652a2.3169 2.3169 0 0 1 2.2222 1.8652h-4.4444a2.3169 2.3169 0 0 1 2.2222-1.8652zm5.37 11.6969a1.0182 1.0182 0 0 1 -1.0166 1.0171h-8.7069a1.0182 1.0182 0 0 1 -1.0165-1.0171v-7.7155a1.0178 1.0178 0 0 1 1.0166-1.0166h8.707a1.0178 1.0178 0 0 1 1.0164 1.0166z"
+            ></path>
+          </svg>
+        </Link> */}
       </div>
 
       {/* Mobile? */}
